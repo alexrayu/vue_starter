dc12ce7bd8e773ecbb5009fbd5426e1f7b625999
Alexei Raiu <alex.rayu@gmail.com>
2019-12-03 10:52:51 +0200

    Add the original router example into the app.

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,6 +2,11 @@
   <div id="app">
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
+    <p>
+      <router-link to="/foo">Go to Foo</router-link>
+      <router-link to="/bar">Go to Bar</router-link>
+    </p>
+    <router-view></router-view>
   </div>
 </template>
 
--- app/src/main.js
+++ app/src/main.js
@@ -4,6 +4,8 @@ import VueRouter from 'vue-router';
 
 // Create routes
 Vue.use(VueRouter);
+const Foo = { template: '<div>foo</div>' };
+const Bar = { template: '<div>bar</div>' };
 const routes = [
   { path: '/foo', component: Foo },
   { path: '/bar', component: Bar }
