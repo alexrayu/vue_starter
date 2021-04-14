25bca9801f504cc39292f83d2a3c80c3200f92fd
Alexei Raiu <alex.rayu@gmail.com>
2021-04-14 14:01:26 +0300

    Add xhr function.

--- app/src/main.js
+++ app/src/main.js
@@ -33,6 +33,60 @@ const routes = [
   { name: 'bar', path: '/bar', component: Bar }
 ];
 
+/**
+ * Send xhr to the server.
+ *
+ * @param params
+ *   Parameters to be passed with the xhr.
+ * @param callback
+ *   Callback.
+ */
+Vue.prototype.xhr = function (params, callback) {
+  let uri = '/my_module/xhr';
+  let xhr = new XMLHttpRequest();
+  let pack = new FormData();
+  let token = drupalSettings.my_module.token || '';
+  for (let i in params) {
+    pack.append(i, params[i]);
+  }
+  
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        let data = JSON.parse(xhr.responseText);
+        if (typeof data.error === 'undefined') {
+          callback({request: params, data: data});
+        }
+        else {
+          callback({
+            request: params,
+            data: {
+              'error': true,
+              'message': 'Error: ' + data.error
+            }
+          });
+        }
+      }
+      else {
+        callback({
+          request: params,
+          data: {
+            'error': true,
+            'message': 'Error ' + this.status + ', "' + this.statusText + '"'
+          }
+        });
+      }
+    }
+  };
+  if (token) {
+    xhr.open('POST', uri + '?token=' + token);
+  }
+  else {
+    xhr.open('POST', uri);
+  }
+  xhr.send(pack);
+};
+
 // Create a root instance
 const vueElement = document.getElementById('vue_starter_app');
 if (typeof vueElement === 'object') {
