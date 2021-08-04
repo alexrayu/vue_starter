792629fc41f3659a515d4d73be8d397144eb1be3
Alexei Raiu <alex.rayu@gmail.com>
2021-08-04 13:24:42 +0300

    Better initial components.

--- app/src/components/Bar.vue
+++ app/src/components/Bar.vue
@@ -1,3 +1,28 @@
 <template>
   <h1>Bar</h1>
 </template>
+
+<script>
+import {Helper} from './main.js';
+import {bus} from './components/bus.js';
+export default {
+  name: 'app',
+  data() {
+    return {
+      Helper,
+      t: Helper.Drupal.t,
+      items: [],
+      isLoading: false,
+      status: null,
+      error: null,
+    }
+  },
+  created() {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
